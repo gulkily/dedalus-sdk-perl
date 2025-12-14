fb4a0a5df78a0f5aecca412be8af475c8c11cd28
Ilya Gulko <ilya@ilyagulko.com>
2025-12-13 17:44:51 -0800

    fix(responses): clean up example output parsing

--- examples/create_response.pl
+++ examples/create_response.pl
@@ -3,29 +3,38 @@ use strict;
 use warnings;
 
 use Dedalus;
+use Try::Tiny;
 
 my $prompt = shift @ARGV // 'Summarize Dedalus in one sentence.';
 my $model  = $ENV{DEDALUS_RESPONSE_MODEL} // 'openai/gpt-5-nano';
 
 my $client = Dedalus->new();
 
-my $response = $client->responses->create(
-    model => $model,
-    input => [
-        { role => 'user', content => $prompt },
-    ],
-);
+my $response = try {
+    $client->responses->create(
+        model => $model,
+        input => [
+            { role => 'user', content => $prompt },
+        ],
+    );
+} catch {
+    die "Error creating response: $_";
+};
 
 print "Response ID: " . $response->id . "\n";
 if (my $output = $response->output) {
     for my $item (@{$output}) {
-        next unless ref $item eq 'HASH' && $item->{type} && $item->{type} eq 'message';
+        next unless ref $item eq 'HASH';
+        next unless ($item->{type} // '') eq 'message';
         my $content = $item->{content};
         if (ref $content eq 'ARRAY') {
-            for my $block (@$content) {
-                print ($block->{text} // ''), "\n" if ref $block eq 'HASH';
+            for my $block (@{$content}) {
+                next unless ref $block eq 'HASH';
+                my $text = $block->{text};
+                next unless defined $text;
+                print $text, "\n";
             }
-        } elsif (!ref $content) {
+        } elsif (!ref $content && defined $content) {
             print $content, "\n";
         }
     }
